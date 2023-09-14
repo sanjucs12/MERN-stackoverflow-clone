d2721937cd9f2b41bd16ab41de327d2efd153ec0
sanjucs12 <sanjucs12@gmail.com>
2023-09-14 23:41:44 +0530

    Backend - code for auth (signup amd login)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,61 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import users from "../models/auth.js";
 
-export const signup = async (req, res, next) => {};
+export const signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: `ALL FIELDS ARE REQUIRED` });
+    }
 
-export const login = async (req, res, next) => {};
+    const existingUser = await users.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: `USER ALREADY EXISTS` });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 12); //12 refers to salt rounds;
+      const newUser = await users.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      const token = jwt.sign(
+        { email: newUser.email, id: newUser._id },
+        "test",
+        { expiresIn: "1h" }
+      );
+      res.status(201).json({ message: `SIGNUP SUCCESSFULL`, token: token });
+    }
+  } catch (err) {
+    res.status(500).json({ message: `SOMETHING WENT WRONG` });
+    console.log(`ERROR IN LINE : 31 : CONTROLLERS/AUTH.JS ${err}`);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "PLEASE ENTER THE REQUIRED DETAILS FOR LOGIN" });
+    }
+    const existingUser = await users.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(404).json({ message: `USER DOES'NT EXISTS` });
+    }
+    const isPasswordCrt = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCrt) {
+      return res.status(401).json({ message: `INCORRECT PASSWORD` });
+    }
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+    res.status().json({ token: token });
+  } catch (err) {
+    res.status(500).json({ message: `SOMENTHING WENT WRONG` });
+    console.log(`ERROR IN LINE 59 : CONTROLLERS/AUTH.JS : ${err}`);
+  }
+};
